af0ab958eb2688a0812d2c947e24f3081c1dea87
Tisiqueira <tiago.m.siqueira@outlook.com>
2023-08-10 00:14:58 -0300

    first commit

---
+++ frontend/README.md
@@ -0,0 +1,16 @@
+##Ferramentas:
+1. Vite
+2. Tailwindcss
+3. Yarn
+
+###Vite:
+*https://vitejs.dev/
+*Escolhido para facilitar o projeto, pois não vamos utilizar todas as funcionalidades do NEST ou seja não faz sentido utilizar para esse projeto.
+
+###Tailwindcss:
+*https://tailwindcss.com/
+	1. Facilitar a produtividade e personalização.
+	2. Theme com configuração ja definida.
+	Nenhum código rodando em runtime - isso não é o problema, mas zero-runtime é melhor.
+	3. Build apenas com as classes que utilizamos no nosso código.
+
---
+++ frontend/src/Router/index.tsx
@@ -0,0 +1,27 @@
+import { Routes, Route, BrowserRouter} from 'react-router-dom'
+
+import { AuthGuard } from './AuthGuard'
+import { Login } from '../view/pages/Login'
+import { Register } from '../view/pages/Dashboard'
+import { Dashboard } from '../view/pages/Register'
+import { AuthLayout } from '../view/layouts/AuthLayout'
+
+export function Router(){
+   return (
+    <BrowserRouter>
+      <Routes>
+        <Route element= {<AuthGuard isPrivate={false}/>} >
+          <Route element={<AuthLayout />}>
+            <Route path='/login' element={<Login />}/>
+            <Route path='/register' element={< Register />}/>
+          </Route>
+        </Route>
+
+        <Route element= {<AuthGuard isPrivate= { true}/>}>
+          <Route path='/' element={<Dashboard />}/>
+        </Route>
+
+      </Routes>
+    </BrowserRouter>
+  )
+}
